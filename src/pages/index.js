f250c11ec22011bf4a61c668a4760511ca832ae6
Shubham <aryan100jangid@gmail.com>
2023-06-12 18:03:39 +0530

    Make navbar fixed, fix social media icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,12 +15,12 @@ export default function Home() {
         <link rel="icon" href="./port.jpg"/>
       </Head>
       <main className="bg-white px-5 dark:bg-gray-800">
-        <section className="min-h-screen">
-          <nav className="py-5 mb-0 flex justify-between sticky top-0 z-50 bg-white dark:bg-gray-800">
-            <h1 className="text-2xl font-burtons dark:text-white"><a href="#">Developed by Shubham</a></h1>
+        <section className="min-h-screen min-w-full">
+          <nav className="py-5 mb-0 flex justify-between fixed top-0 w-full z-50 bg-white dark:bg-gray-800">
+            <h1 className="text-2xl font-burtons scroll-smooth dark:text-white"><a href="#">Developed by Shubham</a></h1>
             <ul className="flex items-center gap-5">
               <li className="cursor-pointer text-xl dark:text-white"><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)}/></li>
-              <li><a href="https://drive.google.com/file/d/15TLi3-uss6GC1rAUBF5apNz0kvNS3jZ4/view" target="_blank" className="text-xl bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-lg">Resume</a></li>
+              <li><a href="https://drive.google.com/file/d/15TLi3-uss6GC1rAUBF5apNz0kvNS3jZ4/view" target="_blank" className="text-xl bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 mr-8 py-2 rounded-md">Resume</a></li>
             </ul>
           </nav>
 
@@ -30,7 +30,7 @@ export default function Home() {
                 <h3 className="text-xl py-2">Open Source Developer</h3>
                 <p className="text-mg py-5 leading-8">Dynamic and skilled software developer with expertise in C++, Python, Java, React, Bootstrap, and the Qt framework. Passionate about open source contributions and actively engaged in the open source community. Regularly practices problem-solving skills through platforms like LeetCode. Looking for a full time Software Developer role, feel free to contact me.</p>
                 
-                <div className="text-5xl flex justify-center gap-12 py-5 text-gray-800 dark:text-white">
+                <div className="text-5xl flex justify-center gap-8 py-5 text-gray-800 dark:text-white">
                   <a href="https://www.linkedin.com/in/shubham-686615155/" target="_blank" className="hover:text-6xl hover:delay-150"><AiFillLinkedin/></a>
                   <a href="https://twitter.com/AryanJangid20" target="_blank" className="hover:text-6xl hover:delay-150"><AiFillTwitterCircle/></a>
                   <a href="mailto:aryan10jangid@gmail.com" className="hover:text-6xl hover:delay-150"><SiGmail/></a>
