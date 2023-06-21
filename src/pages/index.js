8920330389e287c99b124c1dfdea4bb1ad66ac12
Shubham <aryan100jangid@gmail.com>
2023-06-21 23:04:10 +0530

    Fix CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,9 +29,9 @@ export default function Home() {
           <div className="dark:text-white mx-auto md:shadow-2xl md:rounded-2xl md:max-w-xl">
               <div className="text-center p-5">
                 <h2 className="text-6xl pt-14 text-teal-500 font-bold">Shubham</h2>
-                <h3 className="text-xl py-2 md:text-2xl">Open Source Developer</h3>
+                <h3 className="text-lg py-2 md:text-xl">Open Source Developer</h3>
                 <p className="text-md py-5 leading-8 md:text-md max-w-xl mx-auto">A Two time Google Summer of Code student, dynamic and skilled software developer with expertise in C++, Python, Javascipt, Java, React, Tailwind and Bootstrap framework. Passionate about open source software and actively engaged in the open source community. Regularly practices problem-solving skills through platforms like LeetCode. Looking for a full time Software Developer role, feel free to contact me.</p>
-                <div className="text-5xl flex justify-center gap-6 py-5 text-gray-800 dark:text-white">
+                <div className="text-3xl md:text-5xl flex justify-center gap-6 py-5 text-gray-800 dark:text-white">
                   <a href="https://www.linkedin.com/in/shubham-686615155/" target="_blank" className="hover:text-6xl hover:delay-150"><AiFillLinkedin/></a>
                   <a href="https://twitter.com/AryanJangid20" target="_blank" className="hover:text-6xl hover:delay-150"><AiFillTwitterCircle/></a>
                   <a href="mailto:aryan10jangid@gmail.com" className="hover:text-6xl hover:delay-150"><SiGmail/></a>
@@ -39,15 +39,10 @@ export default function Home() {
                   <a href="https://leetcode.com/aryan10jangid/" target="_blank" className="hover:text-6xl hover:delay-150"><SiLeetcode/></a>
                 </div>
               </div>
-              {/* <div className="relative bg-gradient-to-b from-cyan-500 to-teal-500 rounded-full w-80 h-80 mx-auto overflow-hidden mt-0 md:w-96 md:h-96">
-                {
-                  darkMode ? <img alt="Night Coding" src="https://raw.githubusercontent.com/AVS1508/AVS1508/master/assets/Night-Coding.gif"/> : <Image src={deved} layout='fill'/>
-                }
-              </div> */}
 
               {
-                darkMode ? <div className="">
-                  <img alt="Night Coding" src="https://raw.githubusercontent.com/AVS1508/AVS1508/master/assets/Night-Coding.gif" className="w-80 h-64 md:w-96 md:h-64"/></div>
+                darkMode ? <div>
+                  <img alt="Night Coding" src="https://raw.githubusercontent.com/AVS1508/AVS1508/master/assets/Night-Coding.gif" className="mx-auto pb-5 w-80 h-64 md:w-96 md:h-64"/></div>
                   :
                   <div className="relative bg-gradient-to-b from-cyan-500 to-teal-500 rounded-full w-80 h-80 mx-auto overflow-hidden mt-0 md:w-96 md:h-96">
                     <Image src={deved} layout='fill'/>
