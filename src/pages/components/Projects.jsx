b6dfb616a69547a0159e093f561f351067465877
Shubham <aryan100jangid@gmail.com>
2023-06-23 23:02:57 +0530

    Fix image loading

--- src/pages/components/Projects.jsx
+++ src/pages/components/Projects.jsx
@@ -7,8 +7,7 @@ const Projects = () => {
             <h1 className="text-3xl md:text-5xl text-center text-teal-500  font-semibold py-0  pb-5 justify-center">Projects</h1>
             <h5 className="text-2xl font-medium py-5 text-center text-black dark:text-white">YouTubeMate</h5>
             <div className="flex flex-col flex-wrap gap-8  py-0 text-black  dark:text-white">
-              {/* <img src="yt.png" width="100%" height="100%" className="mx-auto border-solid border-black dark:border-white border-4 hover:border-solid"/> */}
-              <Image src="/ant.png" width={600} height={300} className="mx-auto border-solid border-black dark:border-white border-4 hover:border-solid overflow-hidden mt-0 md:w-96 md:h-64"/>
+              <Image src="/ytm.jpg" width={600} height={250} className="mx-auto border-solid border-black dark:border-white border-4 hover:border-solid overflow-hidden mt-0 md:w-96 md:h-64"/>
               <ul className="flex flex-row items-center justify-center gap-4">
                 <li><a href="https://youtubemate1.netlify.app/" target="_blank" className="text-xl bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 mr-4 py-2 rounded-md">Visit Site</a></li>
                 <li><a href="https://github.com/Shubham-100/YouTubeMate" target="_blank"><img src="./github.png" width="32" height="32"/></a></li>
@@ -20,8 +19,7 @@ Users can search for content of their preference based on genre</p>
 
             <h5 className="text-2xl font-medium py-5 text-center dark:text-white text-teal-500">Movies Recommendation App</h5>
             <div className="flex flex-col flex-wrap gap-8  py-0 text-black  dark:text-white">
-              {/* <img src="net.png" width="100%" height="100%" className="mx-auto border-solid border-black dark:border-white border-4 hover:border-solid" /> */}
-              <Image src="/net.png" width={600} height={300} className="mx-auto border-solid border-black dark:border-white border-4 hover:border-solid overflow-hidden mt-0 md:w-96 md:h-64"/>
+              <Image src="/flix.jpg" width={600} height={250} className="mx-auto border-solid border-black dark:border-white border-4 hover:border-solid overflow-hidden mt-0 md:w-96 md:h-64"/>
               <ul className="flex flex-row items-center justify-center gap-4">
                 <li><a href="https://startling-pothos-879818.netlify.app/" target="_blank" className="text-xl bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 mr-4 py-2 rounded-md">Visit Site</a></li>
                 <li><a href="https://github.com/Shubham-100/Movies-TV-shows" target="_blank"><img src="./github.png" width="32" height="32"/></a></li>
