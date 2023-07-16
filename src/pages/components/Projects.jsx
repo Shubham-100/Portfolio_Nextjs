d6546d0186858f6fee1289f01ddfe973d848517c
Shubham <aryan100jangid@gmail.com>
2023-07-16 12:00:09 +0530

    Fix image issue

--- src/pages/components/Projects.jsx
+++ src/pages/components/Projects.jsx
@@ -7,7 +7,8 @@ const Projects = () => {
           <h1 className="text-3xl md:text-5xl text-center text-teal-500  font-semibold py-0  pb-5 justify-center">Projects</h1>
           <h5 className="text-2xl font-medium py-5 text-center text-black dark:text-white ">Movies Recommendation App</h5>
             <div className="flex flex-col flex-wrap gap-8  py-0 text-black  dark:text-white">
-              <Image src="/flix.jpg" alt="project" width={600} height={250} className="object-cover mx-auto border-solid border-black dark:border-white border-4 hover:border-solid overflow-hidden mt-0 md:w-96 md:h-64"/>
+              <Image src="/netflix.png" priority={true} alt="project" width={600} height={250} className="object-cover mx-auto border-solid border-black dark:border-white border-4 hover:border-solid overflow-hidden mt-0 md:w-96 md:h-64"/>
+              {/* <img src="../../../public/netflix.png" className="object-cover mx-auto border-solid border-black dark:border-white border-4 hover:border-solid overflow-hidden mt-0 md:w-96 md:h-64"/> */}
               <ul className="flex flex-row items-center justify-center gap-4">
                 <li><a href="https://enter10ment.vercel.app/" target="_blank" className="text-xl bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 mr-4 py-2 rounded-md">Visit Site</a></li>
                 <li><a href="https://github.com/Shubham-100/Netflix" target="_blank"><img src="./github.png" width="32" height="32"/></a></li>
@@ -20,7 +21,7 @@ const Projects = () => {
             
             <h5 className="text-2xl font-medium py-5 text-center text-black dark:text-white">YouTubeMate</h5>
             <div className="flex flex-col flex-wrap gap-8  py-0 text-black  dark:text-white">
-              <Image src="/ytm.jpg" alt="project" width={600} height={250} className="object-cover mx-auto border-solid border-black dark:border-white border-4 hover:border-solid overflow-hidden mt-0 md:w-96 md:h-64"/>
+              <Image src="/ytm.jpg" priority={false} alt="project" width={600} height={250} className="object-cover mx-auto border-solid border-black dark:border-white border-4 hover:border-solid overflow-hidden mt-0 md:w-96 md:h-64"/>
               <ul className="flex flex-row items-center justify-center gap-4">
                 <li><a href="https://youtubemate1.netlify.app/" target="_blank" className="text-xl bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 mr-4 py-2 rounded-md">Visit Site</a></li>
                 <li><a href="https://github.com/Shubham-100/YouTubeMate" target="_blank"><img src="./github.png" width="32" height="32"/></a></li>
