df1b683b87cfb533a1e3f4f2862e7e8b2256a3d0
Shubham <aryan100jangid@gmail.com>
2023-07-19 16:40:11 +0530

    Fix resume link

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({darkMode, setDarkMode}) => {
             <h1 className=" text-2xl font-burtons scroll-smooth dark:text-white"><a href="#">Developed by Shubham</a></h1>
             <ul className="flex items-center mt-0 gap-2 md:gap-6">
               <li className="cursor-pointer text-xl dark:text-white"><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)}/></li>
-              <li><a href="https://drive.google.com/file/d/15TLi3-uss6GC1rAUBF5apNz0kvNS3jZ4/view" target="_blank" className="text-xl bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 mr-8 py-2 rounded-md">Resume</a></li>
+              <li><a href="https://drive.google.com/file/d/1qU3Nw285S56FDHEE1UC3pyW8iKsqcLzR/view?usp=sharing" target="_blank" className="text-xl bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 mr-8 py-2 rounded-md">Resume</a></li>
             </ul>
           </nav>
     );
